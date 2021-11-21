ec7c3bada409a906c1dbdedd937b94d6e2122438
Ryan Kotsovolos <rkotsovolos@gmail.com>
2021-11-20 17:22:29 -0800

    commit tree 500cc81118b6ac07890dad3e6e88f38321a95191

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+
+def greet_programmer  
+    puts "Hello, programmer!"
+end
+greet_programmer 
+
+
+def greet name 
+    puts "Hello, #{name}!"
+end
+greet "Naureen" 
+
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+
+def add num1, num2 
+    return num1 + num2
+end
+
+sum1 = add(1,1)
+
+
+def halve num
+    return nil unless num.class == Integer
+    num / 2
+end
+
+
+
